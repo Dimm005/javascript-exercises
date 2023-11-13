7c5b3a72ec31993d6c775b94d41b2d4ffcaf552d
Dimm005 <dimm005@gmail.com>
2023-11-13 23:00:06 +0800

    Solve 08 and 09

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,5 +1,13 @@
-const palindromes = function () {
-
+const palindromes = function (string) {
+    string = string.toLowerCase();
+    let reg = /[a-z0-9]/g;
+    let array = string.match(reg);
+    string = array.join("");
+    let string2 = "";
+    for(let i = array.length - 1; i >= 0; i--) {
+        string2 += array[i];
+    };
+    return string === string2;
 };
 
 // Do not edit below this line
