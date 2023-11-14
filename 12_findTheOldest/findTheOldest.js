4ff8c7df0b35191da9b2102a4cec3f3f76a62eb2
Dimm005 <dimm005@gmail.com>
2023-11-14 20:21:22 +0800

    Add solution for the final exercise

--- 12_findTheOldest/findTheOldest.js
+++ 12_findTheOldest/findTheOldest.js
@@ -1,5 +1,26 @@
-const findTheOldest = function() {
-
+const findTheOldest = function(people) {
+    const time = new Date;
+    people.forEach(person => {
+        if (person.yearOfDeath === undefined) {
+            person.yearOfDeath = time.getFullYear();
+        };
+        person.age = person.yearOfDeath - person.yearOfBirth
+        
+    });
+    people.sort((a, b) => {
+        let ageA = a.age;
+        let ageB = b.age;
+        if (ageA > ageB) {
+            return -1;
+        };
+        if (ageA > ageB) {
+            return 1;
+        };
+        if (ageA == ageB) {
+            return 0;
+        };
+    });
+    return people[0];
 };
 
 // Do not edit below this line
