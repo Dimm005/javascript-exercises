db087c94ccd6b7446c56952c0d358d5a76e41a2f
Dimm005 <dimm005@gmail.com>
2023-11-14 19:07:44 +0800

    Add solution for exercise 10

--- 10_fibonacci/fibonacci.js
+++ 10_fibonacci/fibonacci.js
@@ -1,5 +1,20 @@
-const fibonacci = function() {
-
+const fibonacci = function(n) {
+    if (n < 1) {
+        return "OOPS";
+    };
+    if (typeof(n) === "string") {
+        n = parseInt(n);
+    } else if (typeof(n) != "number") {
+        return "OOPS";
+    };
+    let fibArray = [1, 1, 2];
+    let i = 2;
+    let sum = 0;
+    while(fibArray.length <= n) {
+        fibArray.push(fibArray[i - 1] + fibArray[i]);
+        i++;
+    };
+    return fibArray[n-1];
 };
 
 // Do not edit below this line
